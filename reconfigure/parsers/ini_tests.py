76804f728d6cdf36b54c23e138ccf9af65d55c85
Eugeny Pankov <e@ajenti.org>
2012-10-09 21:10:17 +0300

    Fixed tests

--- reconfigure/parsers/ini_tests.py
+++ reconfigure/parsers/ini_tests.py
@@ -6,13 +6,15 @@ import unittest
 
 class IniParserTest (unittest.TestCase):
     def test_parse_stringify(self):
-        content = """[section1]
-s1p1 = asd
-s1p2 = 123
-s1p3 = asd=543
+        content = """
+            [section1]
+            s1p1 = asd
+            s1p2 = 123
+            s1p3 = asd=543
 
-[section2]
-юни = код"""
+            [section2]
+            юни = код
+        """
 
         parser = IniFileParser()
         tree = parser.parse(content)
@@ -20,15 +22,17 @@ s1p3 = asd=543
         self.assertEqual(newcontent.split(), content.split())
 
     def test_sectionless_parse_stringify(self):
-        content = """KEYMAP = us
-FONT = lat9w-16
-FONT_MAP = 8859-1_to_uni"""
+        content = """
+            KEYMAP = us
+            FONT = lat9w-16
+            FONT_MAP = 8859-1_to_uni
+        """
 
         parser = IniFileParser(True)
         tree = parser.parse(content)
         self.assertTrue(tree.get(None) is not None)
         newcontent = parser.stringify(tree)
-        self.assertEqual(filter(None, newcontent.split('\n')), filter(None, content.split('\n')))
+        self.assertEqual(newcontent.split(), content.split())
 
     def test_raise(self):
         tree = RootNode()
