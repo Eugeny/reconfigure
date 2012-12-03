d36650b079bb11749de14b99eafb4ace68b69281
Valentin Bryukhanov <bvv.mag@gmail.com>
2012-12-03 19:24:55 +1200

    Fix imports in parsers.__init__

--- reconfigure/parsers/__init__.py
+++ reconfigure/parsers/__init__.py
@@ -1,19 +1,21 @@
 from base import BaseParser
-from fstab import FSTabParser
-from hosts import HostsParser
+# from fstab import FSTabParser
+# from hosts import HostsParser
 from ini import IniFileParser
 from jsonparser import JsonParser
 from nginx import NginxParser
-from resolv import ResolvParser
+# from resolv import ResolvParser
 from ssv import SSVParser
+from crontab import CrontabParser
 
 __all__ = [
     'BaseParser',
-    'FSTabParser',
-    'HostsParser',
+    # 'FSTabParser',
+    # 'HostsParser',
     'IniFileParser',
     'JsonParser',
     'NginxParser',
-    'ResolvParser',
+    # 'ResolvParser',
     'SSVParser',
+    'CrontabParser',
 ]
