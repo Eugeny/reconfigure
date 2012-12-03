8db93471e18fcd8d9b4bcd00251c6a0189d99663
Valentin Bryukhanov <bvv.mag@gmail.com>
2012-12-03 19:24:03 +1200

    Add crontab parsing test

---
+++ reconfigure/parsers/crontab_tests.py
@@ -0,0 +1,55 @@
+from reconfigure.parsers import CrontabParser
+from reconfigure.nodes import RootNode, Node, PropertyNode
+import unittest
+
+class CrontabParserTest (unittest.TestCase):
+    def setUp(self):
+        self.content = '\n'.join(['#comment line', 
+                            '* * * * * date',
+                            '@reboot ls -al',
+                            '1 * 0 1 2 date -s',
+                            'NAME = TEST',
+                            ])
+        self.tree = RootNode(None, 
+            [
+                PropertyNode('comment', 'comment line'),
+                Node('task', 
+                    [
+                        PropertyNode('minute', '*'),
+                        PropertyNode('hour', '*'),
+                        PropertyNode('day_of_month', '*'),
+                        PropertyNode('month', '*'),
+                        PropertyNode('day_of_week', '*'),
+                        PropertyNode('command', 'date'),
+                    ]
+                ),
+                Node('special',
+                    [
+                        PropertyNode('@reboot', 'ls -al'),
+                    ]
+                ),
+                Node('task', 
+                    [
+                        PropertyNode('minute', '1'),
+                        PropertyNode('hour', '*'),
+                        PropertyNode('day_of_month', '0'),
+                        PropertyNode('month', '1'),
+                        PropertyNode('day_of_week', '2'),
+                        PropertyNode('command', 'date -s'),
+                    ]
+                ),
+                Node('env_setting',
+                    [
+                        PropertyNode('NAME', 'TEST'),
+                    ]
+                ),
+            ]
+        )
+        
+
+    def test_parse(self):
+        parser = CrontabParser()
+        parsed_tree = parser.parse(self.content)
+        self.assertEqual(str(parsed_tree), str(self.tree))
+
+        
