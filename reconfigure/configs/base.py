b3877691fcf42d5f9fe474837dcb11d3a12466db
Xéfir Destiny <xefir@crystalyx.net>
2016-02-28 21:12:58 +0100

    You're right travis !

--- reconfigure/configs/base.py
+++ reconfigure/configs/base.py
@@ -37,8 +37,8 @@ class Reconfig (object):
             self.content = open(self.origin, 'r').read()
 
         self.encoding = 'utf8'
-        if (sys.version_info.major >= 3 and isinstance(self.content, bytes)) or \
-                (sys.version_info.major == 2 and isinstance(self.content, str)):
+        if (sys.version_info[0] >= 3 and isinstance(self.content, bytes)) or \
+                (sys.version_info[0] == 2 and isinstance(self.content, str)):
             try:
                 self.content = self.content.decode('utf8')
             except (UnicodeDecodeError, AttributeError):
--- reconfigure/parsers/ini.py
+++ reconfigure/parsers/ini.py
@@ -69,7 +69,7 @@ class IniFileParser (BaseParser):
             if hasattr(cp[sectionname], '_lines'):
                 self._set_comment(cp[sectionname]._lines[0], section.comment)
 
-        data = (str if sys.version_info.major >= 3 else unicode)(cp) + u'\n'
+        data = (str if sys.version_info[0] >= 3 else unicode)(cp) + u'\n'
         if self.sectionless:
             data = data.replace('[' + self.nullsection + ']\n', '')
         return data
